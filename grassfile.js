8240b7f59a1947c4a0af65b820a485c3e1d31e73
Pein Freecs <plants.coordinators@gmail.com>
2020-08-24 23:23:52 +0800

    Update grassfile.js

--- grassfile.js
+++ grassfile.js
@@ -1,34 +1,3 @@
-var list_package_full_js =  [
-            "src/window_open.js",
-            "src/core.js",
-            "src/boot_dom.js",
-            "src/boot_config.js",
-            "src/boot_ready.js",
-            "src/boot_element.js",
-            "src/boot_ajax.js",
-            "src/boot_agenttype.js",
-            "src/boot_amd.js",
-            "src/boot_init.js",
-            "src/boot_extension.js",
-            "src/functional_helper.js",
-            "src/functional_package_helper.js",
-            "src/functional_extension.js",
-            "src/functional.js",
-           
-  
-  
-];
-
-var list_package_utility_js =  [ 
-              "src/window_open.js",
-              "src/functional_helper.js",
-              "src/functional_package_helper.js",
-              "src/functional_extension.js",
-              "src/functional.js",
-  
-  
-  ];
-
   var list_package_mvc_js =  [ 
     "src/window_open.js",
     "src/mvc_core_library.js",
@@ -50,48 +19,7 @@ exports.module=function(grassconf){
 var grass_concat = grassconf.require("grass_concat")
 var grass_composer = grassconf.require("grass_composer");
 
-        grassconf.load("js_full_package",function(){
-            
-              return grassconf.src(list_package_full_js)
-                .pipe(grass_composer( {
-                  "banner":{
-                      "header":"(function(window){ \n" +
-                      " /** \n" +
-                      " /* This program was writtern by pein freccs. \n" +
-                      " /* Please check my repository for more details and update \n" +
-                      " /* https://github.com/compts/compts \n"+
-                      " **/ \n",
-                  "footer":"\n })(window);"
-                  }
-                } ) )
-                .pipe(grass_concat(__dirname+"/dist/compt-full.js",{
-                  istruncate:true
-                }) );
-            
-
-        })
-
-        grassconf.load("js_utility",function(test){
-
-
-          return grassconf.src(list_package_utility_js)
-            .pipe(grass_composer( {
-              "banner":{
-                  "header":"(function(window){ \n" +
-                  " /** \n" +
-                  " /* This program was writtern by pein freccs. \n" +
-                  " /* Please check my repository for more details and update \n" +
-                  " /* https://github.com/compts/compts \n"+
-                  " **/ \n",
-              "footer":"\n })(window);"
-              }
-            } ) )
-            .pipe(grass_concat(__dirname+"/dist/compt-utility.js",{
-              istruncate:true
-            }) );
-
-  
-        });
+      
 
         grassconf.load("mvvm",function(test){
 
@@ -118,7 +46,7 @@ var grass_composer = grassconf.require("grass_composer");
 
 exports.execute=function( lib ){   
   lib.default=function(strm){
-    strm.series("js_full_package").series("js_utility").series("mvvm");
+    strm.series("mvvm");
   }
    
      
